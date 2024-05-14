a27c49b93edbb203e7da965e1fc90356b8529d8e
Hadiur Rahman <hadiurahman139@gmail.com>
2024-05-14 22:35:39 +0600

    added a delete api for delete the comment that a user want

--- index.js
+++ index.js
@@ -71,6 +71,15 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/allComments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(query);
+        const result = await allCommentsCollection.deleteOne(query);
+
+        res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
